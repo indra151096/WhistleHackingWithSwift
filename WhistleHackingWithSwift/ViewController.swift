c534fc855530132b88f4a06461f83bb130ed49bc
Indra <indra.sumawi@gmail.com>
2019-09-17 17:51:24 +0700

    Initial Commit

---
+++ WhistleHackingWithSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhistleHackingWithSwift
+//
+//  Created by Indra Sumawi on 17/09/19.
+//  Copyright © 2019 Indra Sumawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
